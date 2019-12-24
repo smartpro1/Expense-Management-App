54cd4321bebaee5232a528d15075ac4bef036826
smartpro1 <akeni.promise@yahoo.com>
2019-12-24 15:08:31 +0100

    insert data into data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into user values(1, 'Promise', 'promise@decagon.com')
+insert into user values(2, 'Ajofotan', 'ajofotan@warri.com')
+insert into user values(3, 'Gideon', 'gideon@decagon.com')
+insert into user values(4, 'Precious', 'precious@warri.com')
+
+insert into category values(1, 'Rent')
+insert into category values(2, 'Transportation')
+insert into category values(3, 'Food')
+insert into category values(4, 'School Fees')
+
+insert into expense values (11, 'Lagos trip', '2019-06-16T17:00:00.000Z','Victoria Island', 1,2)
+insert into expense values (12, 'Food stuff', '2019-06-15T15:00:00.000Z','Ibadan', 2,3)
+insert into expense values (13, 'School fees', '2019-06-14T15:00:00.000Z','Arizona', 3,4)
+insert into expense values (14, 'House rent', '2019-06-14T15:00:00.000Z','Arizona', 1,1)
