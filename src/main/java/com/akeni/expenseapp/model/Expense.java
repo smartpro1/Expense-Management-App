a219c07dbcca0f1be9b45e8aca11aab01cddcb3b
smartpro1 <akeni.promise@yahoo.com>
2019-12-24 16:39:41 +0100

    models tested application running

--- src/main/java/com/akeni/expenseapp/model/Expense.java
+++ src/main/java/com/akeni/expenseapp/model/Expense.java
@@ -5,19 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.Instant;
 
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 @Entity
-@Data
+//@Data
 @Table(name="expense")
 public class Expense {
+    @Id
  private Long id;
- private Instant date;
  private String description;
+ private Instant date;
  private String location;
 
  @ManyToOne
@@ -25,5 +27,7 @@ public class Expense {
 
  @ManyToOne
     private Category category;
+
+
 
 }
