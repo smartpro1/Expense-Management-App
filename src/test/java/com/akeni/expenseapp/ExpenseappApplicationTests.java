8f80a91875b838580da4dc41b7b03e5d5f2ab7c4
smartpro1 <akeni.promise@yahoo.com>
2019-12-24 14:06:17 +0100

    initial commit

---
+++ src/test/java/com/akeni/expenseapp/ExpenseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akeni.expenseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
